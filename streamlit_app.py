1335fcb872f7aa55b18e327a4a6c2a0c686c01ed
Ihor Markevych <vukpud@gmail.com>
2020-12-03 10:57:50 -0500

    Revert "sampling fixed"
    This reverts commit e72fe3f2d89517caf48c7d1fc0b0983ceb187518.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
+import re
+import umap
+import nltk
 import pickle
+import string
 import numpy as np
 import pandas as pd
 import altair as alt
@@ -16,6 +20,61 @@ BUFFER_SIZE = 10000
 BATCH_SIZE = 64
 SEED = 100100
 
+
+# for processing text
+try:
+    nltk.data.find("stopwords")
+except:
+    nltk.download("stopwords", quiet = True)
+try:
+    nltk.data.find("wordnet")
+except:
+    nltk.download("wordnet", quiet = True)
+try:
+    nltk.data.find("punkt")
+except:
+    nltk.download("punkt", quiet = True)
+try:
+    nltk.data.find('averaged_perceptron_tagger')
+except:
+    nltk.download('averaged_perceptron_tagger', quiet = True)
+
+lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()
+english_stopwords = set(nltk.corpus.stopwords.words('english'))
+
+@st.cache(allow_output_mutation=True)
+def process_text(text):
+    def get_pos(tag):
+        if tag.startswith("J"):
+            return "a"
+        elif tag.startswith("V"):
+            return "v"
+        elif tag.startswith("R"):
+            return "r"
+        else:
+            return "n"
+
+    text = text.replace("<br />", "")
+    text = text.replace("\'", "'")
+
+    text = re.sub(r"'s", "", text.lower())
+    text = re.sub(r"([a-z0-9]+)'([^s])", r"\1\2", text)
+    text = re.sub(rf"[^{string.ascii_letters}0-9]", " ", text)
+
+
+    tokenized = []
+    for token in nltk.word_tokenize(text):
+        token, tag = nltk.pos_tag([token])[0]
+        t = lemmatizer.lemmatize(token, pos=get_pos(tag))
+        if t not in english_stopwords and len(t) > 1:
+            tokenized.append(t)
+    return " ".join(tokenized)
+
+
+def predict(model, sentences):
+    return model.predict(np.array([process_text(s) for s in sentences]))
+
+
 def probability(x):
     return np.round(np.abs(2 * (1 / (1 + np.exp(-x)) - 0.5)), 2)
 
@@ -31,6 +90,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+
 @st.cache(allow_output_mutation=True)
 def load_data(file_name):
     data = pd.read_csv(file_name, index_col=None, sep="\t")
@@ -39,22 +99,21 @@ def load_data(file_name):
 @st.cache(allow_output_mutation=True)
 def load_pickled(file_name):
     with open(file_name, "rb") as r_obj:
-        return pickle.load(r_obj)
+        return pickle.load(r_obj, encoding="utf-8")
 
 @st.cache(allow_output_mutation=True)
 def load_embedding(file_name):
     return np.load(file_name, allow_pickle=True)
 
-@st.cache(allow_output_mutation=True)
+#@st.cache(allow_output_mutation=True)
 def load_main_model():
     dataset = tfds.load('imdb_reviews', as_supervised=True)
-    train_dataset = dataset["train"]
-    train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+    X_train = load_pickled("v.pkl")
 
     VOCAB_SIZE=10000
     encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(
             max_tokens=VOCAB_SIZE)
-    encoder.adapt(train_dataset.map(lambda text, label: text))
+    encoder.adapt(X_train)
 
     model = tf.keras.Sequential([
         encoder,
@@ -71,6 +130,9 @@ def load_main_model():
     ])
 
     model.compile()
+
+    model.load_weights('./training/cp-0001.ckpt')
+
     return model
 
 @st.cache(allow_output_mutation=True)
@@ -86,10 +148,16 @@ def color_text(text, model):
         else:
             return f"<span style='color:green; opacity:{abs(p / max(np.abs(probs))) + 0.2}'>{text}</span>"
 
-    tokens = text.split(" ")
+    tokens = text.split()
     probs = [0]
     for k in range(0, len(tokens)):
-        probs.append(model.predict(np.array([" ".join(tokens[:k+1])]))[0][0])
+        proc_text = process_text(" ".join(tokens[:k+1]))
+        if proc_text == "":
+            probs.append(probs[-1])
+        else:
+            probs.append(
+                predict(model, np.array([proc_text]))[0][0]
+            )
     fin_prob = probs[-1]
     probs = np.diff(probs)
     colored_texts = [make_colored_text(token, p, probs)
@@ -101,15 +169,16 @@ st.title("Explaining RNNs")
 
 main_df = load_data("combined_sentiment_labelled.tsv")
 
-embedding = load_pickled("train_embedding.pkl")
+#embedding = load_pickled("train_embedding.pkl")
 
-#np.random.seed(SEED)
+np.random.seed(SEED)
 
 main_model = load_main_model()
 
 colored = load_pickled('colored.txt')
 
 def sen2vec(x):
+    x = np.array([[process_text(xx[0])] for xx in x])
     return main_model.get_layer(name='embedding')(main_model.get_layer(name="text_vectorization")(x))
 
 sen2vec_model = tf.keras.Sequential([
@@ -130,10 +199,11 @@ n_neighbor = st.slider(
     "Choose the number of neighboring reviews to find",
     min_value=50, max_value=len(main_df), value=50, step=50
     )
+n_neighbor = 3000 #fix
 
-ixs = get_ixs(len(main_df), n_neighbor)
-main_df = main_df.iloc[ixs, :]
-embedding = embedding[ixs, :]
+#ixs = get_ixs(len(main_df), n_neighbor)
+#main_df = main_df.iloc[ixs, :]
+#embedding = embedding[ixs, :]
 
 
 
@@ -165,8 +235,11 @@ if text != "":
 
     sentences = np.append(main_df["text"].values, text)
 
-    for i in range(6):
-        main_model.load_weights(f'./training/cp-000{i}.ckpt')
+    for i in range(0, 11, 2):
+        fname = f'./training/cp-000{i}.ckpt' if i < 10 else f'./training/cp-00{i}.ckpt'
+
+
+        main_model.load_weights(fname)
         pred = color_text(text, model=main_model)
         st.markdown(f"Epoch {i}" + " | " +
                     ("NEG" if pred[1] < 0 else "POS") + " | " +
@@ -174,28 +247,42 @@ if text != "":
                     pred[0],
                     unsafe_allow_html=True)
 
-    for i in range(6):
+    for i in range(0, 11, 2):
         st.markdown(f'#### Epoch {i}')
-        main_model.load_weights(f'./training/cp-000{i}.ckpt')
-        probs = main_model.predict(sentences).reshape(-1).round(2)
+        fname = f'./training/cp-000{i}.ckpt' if i < 10 else f'./training/cp-00{i}.ckpt'
+        main_model.load_weights(fname)
+
+        umap_raw = load_pickled(f'./embedding/umap-raw-{i}.pkl')
+        umap_intermediate = load_pickled(f'./embedding/umap-intermediate-{i}.pkl')
+        umap_proc = load_pickled(f'./embedding/umap-proc-{i}.pkl')
+
+
+        probs = predict(main_model, sentences).reshape(-1).round(2)
         labels = ['Positive' if x else 'Negative'
                   for x in (probs.reshape(-1) > 0)]
         labels[-1] = "User"
-        emb = sen2vec_model.predict([text])[0]
-        embedding_ep = np.append(embedding, [emb], axis=0)
 
-        tsne = TSNE(random_state=SEED, n_jobs=-1)
-        tsned_space_raw_emb = tsne.fit_transform(sen2vec([[x] for x in sentences]).numpy().mean(axis=1))
-        tsned_space_proc_emb = tsne.fit_transform(sen2vec_model.predict(sentences))
-        tsned_space_intermediate_emb = tsne.fit_transform(embedding_ep)
+        raw_emb_text = umap_raw.transform(
+            sen2vec([[text]]).numpy().mean(axis=1)
+            )
+        umap_raw_emb = np.append(umap_raw.embedding_, raw_emb_text, axis=0)
 
-        tsne_plot_data = pd.DataFrame({
-            'x_raw': tsned_space_raw_emb[:,0],
-            'y_raw': tsned_space_raw_emb[:,1],
-            'x_interm': tsned_space_intermediate_emb[:,0],
-            'y_interm': tsned_space_intermediate_emb[:,1],
-            'x_proc': tsned_space_proc_emb[:,0],
-            'y_proc': tsned_space_proc_emb[:,1],
+        intermediate_emb_text = umap_intermediate.transform(
+            predict(sen2vec_model_interm, [text])
+            )
+        umap_intermediate_emb = np.append(umap_intermediate.embedding_,
+                                          intermediate_emb_text,
+                                          axis=0)
+
+        proc_emb_text = umap_proc.transform(predict(sen2vec_model, [text]))
+        umap_proc_emb = np.append(umap_proc.embedding_, proc_emb_text, axis=0)
+        plot_data = pd.DataFrame({
+            'x_raw': umap_raw_emb[:,0],
+            'y_raw': umap_raw_emb[:,1],
+            'x_interm': umap_intermediate_emb[:,0],
+            'y_interm': umap_intermediate_emb[:,1],
+            'x_proc': umap_proc_emb[:,0],
+            'y_proc': umap_proc_emb[:,1],
             'sentence': sentences,
             'opacity': np.abs(probs),
             'prob': probability(probs).astype(str),
@@ -203,7 +290,7 @@ if text != "":
 
         selector_embs = alt.selection_interval(empty='all', encodings=['x', 'y'])
 
-        words_tsned = alt.Chart(tsne_plot_data).mark_circle(size=200).encode(
+        words_tsned = alt.Chart(plot_data).mark_circle(size=200).encode(
             x = 'x_raw',
             y = 'y_raw',
             tooltip =[alt.Tooltip('sentence'), alt.Tooltip('prob')],
@@ -218,7 +305,7 @@ if text != "":
             selector_embs
         )
 
-        interm_tsned = alt.Chart(tsne_plot_data).mark_circle(size=200).encode(
+        interm_tsned = alt.Chart(plot_data).mark_circle(size=200).encode(
             x = 'x_interm',
             y = 'y_interm',
             tooltip =[alt.Tooltip('sentence'), alt.Tooltip('prob')],
@@ -233,7 +320,7 @@ if text != "":
             selector_embs
         )
 
-        sentences_tsned = alt.Chart(tsne_plot_data).mark_circle(size=200).encode(
+        sentences_tsned = alt.Chart(plot_data).mark_circle(size=200).encode(
             x = 'x_proc',
             y = 'y_proc',
             tooltip =[alt.Tooltip('sentence'), alt.Tooltip('prob')],
